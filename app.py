c961aa1dcdb006f1812b3fd169d1f317044b8026
Joao Menes <joao@norton.me>
2024-10-12 15:43:06 -0300

    Correção main

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from modelos.restaurante import Restaurante
 
-mensagem = restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca = Restaurante('praça', 'Gourmet')
 mensagem = restaurante_praca.receber_avaliacao('Gui', 3)
 mensagem = restaurante_praca.receber_avaliacao('Lais', 6)
 mensagem = restaurante_praca.receber_avaliacao('Emy', 15)

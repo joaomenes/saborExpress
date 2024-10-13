60bfc2b2115b5e2bf20c48c827c15a9049783d8c
Joao Menes <joao@norton.me>
2024-10-13 15:06:56 -0300

    Criando a lista do cardápio

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -8,6 +9,7 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
@@ -42,3 +44,7 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
+    
+    def adicionar_no_cardapio(self,item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
